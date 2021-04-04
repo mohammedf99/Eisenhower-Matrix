34e36f3ccda0d4bda6340e74ea7e90652265f778
mohammedf99 <mohammed.hamidf99@gmail.com>
2021-04-04 12:54:38 +0300

    solved message bug

--- main.js
+++ main.js
@@ -1,54 +1,67 @@
-const greenUl = document.querySelector('#green-ul');
-const blueUl = document.querySelector('#blue-ul');
-const orangeUl = document.querySelector('#orange-ul');
-const redUl = document.querySelector('#red-ul');
-const taskForm = document.querySelector('.task-form');
-let msg = document.querySelector('#msg');
+let greenUl = document.querySelector("#green-ul");
+let blueUl = document.querySelector("#blue-ul");
+let orangeUl = document.querySelector("#orange-ul");
+let redUl = document.querySelector("#red-ul");
+let taskForm = document.querySelector(".task-form");
+let msg = document.querySelector(".msg");
 
-const taskTitle = document.querySelector('#newTask');
-const priority = document.querySelector('#taskPriority');
+let taskTitle = document.querySelector("#newTask");
+let priority = document.querySelector("#taskPriority");
 
+taskForm.addEventListener("submit", addTask);
 
-taskForm.addEventListener('submit', addTask);
+function errorMsg() {
+  msg.classList.add("error");
+  msg.innerHTML = "Please fill in both inputs";
+  setTimeout(() => msg.remove(), 3000);
+}
 
-function addTask(e){
-    e.preventDefault();
-    
-    let pValue = priority.value;
+function successMsg() {
+  msg.classList.add("success");
+  msg.innerHTML = "Task added successfuly";
+  setTimeout(() => {
+    msg.innerHTML = "";
+    msg.classList.remove("success");
+  }, 3000);
+}
 
-    if(taskTitle.value == '' || priority.value == ''){
-        msg.classList.add('error');
-        msg.innerHTML = 'please fill out both inputs';
-    }else{
-        msg.classList.add('success');
-        msg.innerHTML = 'Task added successfuly';
-        const li = document.createElement('li');
-        li.appendChild(document.createTextNode(taskTitle.value));
+function addTask(e) {
+  e.preventDefault();
 
-        if(pValue === 'IU'){
+  if (taskTitle.value === "" || priority.value === "") {
+    errorMsg();
+  } else {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(taskTitle.value));
 
-            greenUl.appendChild(li);
-            taskTitle.value = '';
-            priority.value = '';
+    switch (priority.value) {
+      case "IU":
+        greenUl.appendChild(li);
+        successMsg();
+        taskTitle.value = "";
+        priority.value = "";
+        break;
 
-        }else if(pValue === 'INU'){
+      case "INU":
+        blueUl.appendChild(li);
+        successMsg();
+        taskTitle.value = "";
+        priority.value = "";
+        break;
 
-            blueUl.appendChild(li);
-            taskTitle.value = '';
-            priority.value = '';
+      case "NIU":
+        orangeUl.appendChild(li);
+        successMsg();
+        taskTitle.value = "";
+        priority.value = "";
+        break;
 
-        }else if(pValue === 'NIU'){
-
-            orangeUl.appendChild(li);
-            taskTitle.value = '';
-            priority.value = '';
-
-        }else if(pValue === 'NINU'){
-
-            redUl.appendChild(li);
-            taskTitle.value = '';
-            priority.value = '';
-
-        }
+      case "NINU":
+        redUl.appendChild(li);
+        successMsg();
+        taskTitle.value = "";
+        priority.value = "";
+        break;
     }
+  }
 }
